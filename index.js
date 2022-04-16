4d97c61b44e8cc4c6be06ce830e272a34200b9c4
artur.krol <krol.arthurr@gmail.com>
2022-04-16 18:58:22 +0200

    mysql + connection

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+    database: "weatherdb",
+    port : 3306
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
